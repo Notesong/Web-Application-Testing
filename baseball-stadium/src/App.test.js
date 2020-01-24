a169bd573cb73cdfbfb38a73e232875bc5073c70
Lee Damien <notesong.git@outlook.com>
2020-01-24 01:21:04 -0500

    Add tests

--- baseball-stadium/src/App.test.js
+++ baseball-stadium/src/App.test.js
@@ -2,8 +2,38 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('displaying balls heading in display panel', () => {
+  // ARRANGE
+  const expectedElements = 1;
+  const { getAllByText } = render(<App />);
+
+  // ACT
+  const displayElement = getAllByText(/balls/i);
+
+  // ASSERT
+  expect(displayElement.length).toBe(expectedElements);
+});
+
+test('displaying strikes heading in display panel', () => {
+  // ARRANGE
+  const expectedElements = 1;
+  const { getAllByText } = render(<App />);
+
+  // ACT
+  const displayElement = getAllByText(/strikes/i);
+
+  // ASSERT
+  expect(displayElement.length).toBe(expectedElements);
+});
+
+test('displaying 4 buttons in dashboard', () => {
+  // ARRANGE
+  const expectedButtonCount = 4;
+  const { getAllByRole } = render(<App />);
+
+  // ACT
+  const buttonElements = getAllByRole('button');
+
+  // ASSERT
+  expect(buttonElements.length).toBe(expectedButtonCount);
 });
